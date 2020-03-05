9637c053570de48b197f91a377638a4caa9b132a
haski <perepelkin2601@gmail.com>
2020-03-05 10:06:20 +0500

    init

---
+++ TaskDFS/Graph.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskDFS
+{
+    public class Graph
+    {
+        private readonly Node[] nodes;
+
+        public Graph(int size, int[,] adjacencyMatrix)
+        {
+            nodes = new Node[size];
+            for (var i = 1; i <= size; i++) nodes[i - 1] = new Node(i);
+
+            for (var i = 0; i < size; i++)
+            for (var j = 0; j < size; j++)
+                if (adjacencyMatrix[i, j] == 1)
+                    nodes[i].AddNeighbor(nodes[j]);
+        }
+
+        private bool DFS(Node node)
+        {
+            node.IsVisited = true;
+            foreach (var neighbor in node.Neighbors)
+                if (neighbor.IsVisited)
+                {
+                    if (neighbor.Color == node.Color)
+                        return false;
+                }
+                else
+                {
+                    neighbor.InverseColor();
+                    DFS(neighbor);
+                }
+
+            return true;
+        }
+
+        public bool IsBipartite()
+        {
+            return DFS(nodes[0]);
+        }
+
+        public Tuple<Node[], Node[]> GetParts()
+        {
+            var upperPart = new List<Node>();
+            var lowerPart = new List<Node>();
+
+            var upperColor = nodes[0].Color;
+
+            foreach (var node in nodes)
+                if (node.Color == upperColor)
+                    upperPart.Add(node);
+                else lowerPart.Add(node);
+
+            return new Tuple<Node[], Node[]>(upperPart.ToArray(), lowerPart.ToArray());
+        }
+
+        public static string PartToLine(Node[] nodes)
+        {
+            var builder = new StringBuilder();
+            foreach (var node in nodes)
+            {
+                builder.Append(node);
+                builder.Append(' ');
+            }
+            builder.Append('0');
+            return builder.ToString();
+        }
+    }
+
+    public class Node
+    {
+        public Color Color;
+        public bool IsVisited;
+        public readonly List<Node> Neighbors;
+        private readonly int number;
+
+        public Node(int number)
+        {
+            this.number = number;
+            Neighbors = new List<Node>();
+            Color = Color.Black;
+        }
+
+        public void AddNeighbor(Node neighbor)
+        {
+            Neighbors.Add(neighbor);
+        }
+
+        public void InverseColor()
+        {
+            Color = Color == Color.Black ? Color.White : Color.Black;
+        }
+
+        public override string ToString()
+        {
+            return number.ToString();
+        }
+    }
+
+    public enum Color
+    {
+        Black,
+        White
+    }
+}
---
+++ TaskDFS/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace TaskDFS
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+                return;
+
+            using var inputFile = File.OpenText(args[0]);
+
+            var size = int.Parse(inputFile.ReadLine() ?? throw new ArgumentException());
+            var matrix = new int[size, size];
+
+            for (var i = 0; i < size; i++)
+            {
+                var line = inputFile.ReadLine();
+                if (line == null) continue;
+                var numbers = line
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (var j = 0; j < size; j++) matrix[i, j] = numbers[j];
+            }
+
+            using var outputFile = File.CreateText(args[1]);
+            var graph = new Graph(size, matrix);
+
+            if (graph.IsBipartite())
+            {
+                outputFile.WriteLine("Y");
+                var (upper, lower) = graph.GetParts();
+                outputFile.WriteLine(Graph.PartToLine(upper));
+                outputFile.WriteLine(Graph.PartToLine(lower));
+            }
+            else
+            {
+                outputFile.WriteLine("N");
+            }
+        }
+    }
+}
